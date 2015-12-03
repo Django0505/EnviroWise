f938e1a612f92393f9b63eed0c735341ce7d5c63
Mawande06 <mawande@projectcodex.co>
2015-12-03 13:02:39 +0200

    Added compression module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    compression = require('compression'),
     exphbs = require('express-handlebars'),
     mysql = require('mysql'),
     myConnection = require('express-myconnection')
@@ -7,12 +8,12 @@ var express = require('express'),
 app.set('strict routing', true);
 app.set('x-powered-by', false);
 app.set('Admin', false);
-
+app.use(compression())
 //Gives access to our database tables
 var dbOptions = {
      host : "localhost",
      user : "root",
-     password : "coder123",
+     password : "spot",
      port : 3306,
      database : "EnviroWise"
  };
