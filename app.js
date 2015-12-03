2704610697f1b9d15f9e876193a5f050d0e05546
mshava <sakhile@projectcodex.co>
2015-12-03 19:50:17 +0200

    modified app and created signup

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mysql = require('mysql'),
     myConnection = require('express-myconnection'),
-    location = require('./routes/locations')
-    signup = require("./routes/signup"),
-    login = require("./routes/login"),
+    users = require("./routes/users"),
+    signup = require("./routes/signup");
+    login = require("./routes/login")
     app = express();
 
 app.set('strict routing', true);
@@ -17,14 +17,14 @@ app.use(compression());
 var dbOptions = {
      host : "localhost",
      user : "root",
-     password : "coder123",
+     password : "2197832",
      port : 3306,
      database : "EnviroWise"
  };
 
 //Allows us to use mysql from the http request
 app.use(myConnection(mysql, dbOptions, "single"));
-app.use(bodyParser.urlencoded({ extended: false }))
+
  app.use(bodyParser.json());
  app.engine('handlebars', exphbs({
      defaultLayout: 'main'
@@ -32,12 +32,25 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'handlebars');
 app.use(express.static('views'));
 app.use(express.static('public'));
+app.get('/',function(req,res){
+ 	res.render('index')
+ });
+
+app.post("/signup",signup.get);
+app.post("/signup/update/:id",signup.update);
+app.get("/signup/edit/:id",signup.get);
+app.get("/signup/delete:id",signup.delete);
+
+app.get("/users", users.checkUser,users.showUsers);
+app.get("/users/edit/:id",users.checkUser,users.get);
+app.post("/users/update/:id",users.checkUser,users.update);
 
 //Here we rendering the login template to the browser
 app.get("/login", function(req, res){
  res.render("login");
 })
-
+app.get("/login",login.get);
+app.post("/login", login.update);
 //Here we rendering the about template to the browser
 app.get('/about', function(req, res){
   res.render('about');
@@ -61,16 +74,9 @@ app.get('/feedback', function(req, res){
   res.render('feedback');
 })
 
-app.post('/locations/add', location.add);
-app.get('/', location.show);
-app.get('/locations/delete/:id', location.delete);
-
-
 var port = process.env.PORT || 5000;
-
-
 var server = app.listen(port, function() {
     var host = server.address().address
     var port = server.address().port
-    console.log('EnviroWise running at http://localhost:5000/');
+    console.log('EnviroWise running at http://localhost:3000/');
 });
