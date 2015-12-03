c9f2439a72d380c6c978b7a055c3e6c47b80028e
mshava <sakhile@projectcodex.co>
2015-12-03 11:42:08 +0200

    enviroWise script modified

--- enviroWise_script.sql
+++ enviroWise_script.sql
@@ -1,1 +1,20 @@
-Ctrl-C -- exit!
+drop table users if exists;
+drop table bin_status if exists;
+
+create table users(id int not null auto_increment,
+                    users_id int ,
+                    name varchar(100),
+                    username varchar(100),
+                    address varchar(100),
+                    contacts int(11),
+                    userRoles varchar(100),
+                    primary key(id)
+                    );
+create table bin_status(id int not null auto_increment,
+                        bin_full bool,
+                        discription varchar(100);
+                        locations varchar(100),
+                        latitude decimal(10.7),
+                        longitude decimal(10.7),
+                        primary key(id)
+                        );
